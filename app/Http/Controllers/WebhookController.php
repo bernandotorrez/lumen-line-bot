e34d9a6790d0d615adbdc748a9936a279db53967
Bernando Torrez <bernandotorrez4@gmail.com>
2020-12-20 10:38:30 +0700

    update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -195,68 +195,122 @@ class WebhookController extends Controller
 
         $carModel = CarModel::all();
 
-        $contents = array();
+        // $contents = array();
         
-        foreach($carModel as $model) {
-            $x = array(
-                0 => (object) array(
-                    'type' => 'bubble',
-                    'hero' => (object) array(
-                        'type' => 'image',
-                        'url' => $model->nama_model,
-                        'size' => 'full',
-                        'aspectRatio' => '20:13',
-                        'aspectMode' => 'cover',
-                        'action' => (object) array(
-                            'type' => 'message',
-                            'label' => $model->nama_model,
-                            'text' => $model->nama_model
-                        )
-                    ),
-                    'body' => (object) array(
-                        'type' => 'box',
-                        'layout' => 'vertical',
-                        'contents' => array(
-                            0 => (object) array(
-                                'type' => 'text',
-                                'text' => $model->nama_model,
-                                'weight' => 'bold',
-                                'size' => 'xl',
-                                'contents' => array()
+        // foreach($carModel as $model) {
+        //     $x = array(
+        //         0 => (object) array(
+        //             'type' => 'bubble',
+        //             'hero' => (object) array(
+        //                 'type' => 'image',
+        //                 'url' => $model->nama_model,
+        //                 'size' => 'full',
+        //                 'aspectRatio' => '20:13',
+        //                 'aspectMode' => 'cover',
+        //                 'action' => (object) array(
+        //                     'type' => 'message',
+        //                     'label' => $model->nama_model,
+        //                     'text' => $model->nama_model
+        //                 )
+        //             ),
+        //             'body' => (object) array(
+        //                 'type' => 'box',
+        //                 'layout' => 'vertical',
+        //                 'contents' => array(
+        //                     0 => (object) array(
+        //                         'type' => 'text',
+        //                         'text' => $model->nama_model,
+        //                         'weight' => 'bold',
+        //                         'size' => 'xl',
+        //                         'contents' => array()
+        //                     )
+        //                 )
+        //             ),
+        //             'footer' => (object) array(
+        //                 'type' => 'box',
+        //                 'layout' => 'vertical',
+        //                 'flex' => 0,
+        //                 'spacing' => 'sm',
+        //                 'contents' => array(
+        //                     0 => (object) array(
+        //                         'type' => 'button',
+        //                         'action' => (object) array(
+        //                             'type' => 'message',
+        //                             'label' => 'see more',
+        //                             'text' => 'see more'
+        //                         ),
+        //                         'height' => 'sm',
+        //                         'style' => 'link',
+        //                     ),
+        //                     1 => (object) array(
+        //                         'type' => 'spacer',
+        //                         'size' => 'sm'
+        //                     )
+        //                 )
+        //             )
+        //         )
+        //     );
+
+        //     array_push($contents, $x);
+        // }
+
+        $content = (object) array(
+            'type' => 'carousel',
+            'contents' => (object) array(
+                'type' => 'carousel',
+                'contents' => array(
+                    0 => (object) array(
+                        'type' => 'bubble',
+                        'hero' => (object) array(
+                            'type' => 'image',
+                            'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                            'size' => 'full',
+                            'aspectRatio' => '20:13',
+                            'aspectMode' => 'cover',
+                            'action' => (object) array(
+                                'type' => 'uri',
+                                'label' => 'Line',
+                                'uri' => 'https://linecorp.com/'
                             )
-                        )
-                    ),
-                    'footer' => (object) array(
-                        'type' => 'box',
-                        'layout' => 'vertical',
-                        'flex' => 0,
-                        'spacing' => 'sm',
-                        'contents' => array(
-                            0 => (object) array(
-                                'type' => 'button',
-                                'action' => (object) array(
-                                    'type' => 'message',
-                                    'label' => 'see more',
-                                    'text' => 'see more'
+                        ),
+                        'body' => (object) array(
+                            'type' => 'box',
+                            'layout' => 'vertical',
+                            'contents' => array(
+                                0 => (object) array(
+                                    'type' => 'text',
+                                    'text' => 'Brown Cafe',
+                                    'weight' => 'bold',
+                                    'size' => 'xl',
+                                    'contents' => array()
+                                )
+                            )
+                        ),
+                        'footer' => (object) array(
+                            'type' => 'box',
+                            'layout' => 'vertical',
+                            'flex' => 0,
+                            'spacing' => 'sm',
+                            'contents' => array(
+                                0 => (object) array(
+                                    'type' => 'button',
+                                    'action' => (object) array(
+                                        'type' => 'message',
+                                        'label' => 'see more',
+                                        'text' => 'see more'
+                                    ),
+                                    'height' => 'sm',
+                                    'style' => 'link',
                                 ),
-                                'height' => 'sm',
-                                'style' => 'link',
-                            ),
-                            1 => (object) array(
-                                'type' => 'spacer',
-                                'size' => 'sm'
+                                1 => (object) array(
+                                    'type' => 'spacer',
+                                    'size' => 'sm'
+                                )
                             )
                         )
                     )
                 )
-            );
-
-            array_push($contents, $x);
-        }
-
-        $content = (object) array(
-            'type' => 'carousel',
-            'contents' => $contents
+            )
         );
 
         $template = new RawMessageBuilder([
